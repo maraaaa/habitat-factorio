2da87153383f3af6af5165ad71d097ccede94e03
maraaaa <mara.arthie@gmail.com>
2018-04-14 14:33:25 -0600

    use toml-rb instead

--- orig/json_to_toml.rb
+++ orig/json_to_toml.rb
@@ -1,16 +1,15 @@
 #!/usr/bin/env ruby
 
 require 'json'
-require 'toml'
+require 'toml-rb'
 
 file = ARGV.shift
 config = JSON.parse File.read(file)
 
 section_name = File.basename(file, File.extname(file)).sub('.example', '')
-doc = TOML::Generator.new(config).body
+doc = TomlRB.dump ({ section_name => config })
 
 puts '#' * 50
 puts "#!!!!!!!!!!! map-settings start here !!!!!!!!!!!!!"
 puts '#' * 50
-puts "[#{section_name}]"
 puts doc
